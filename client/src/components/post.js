09284975904a1046d5ab4693b8e4b45d176189c2
Jason Lee <jasonjlee.5@gmail.com>
2016-10-26 18:18:35 -0700

    [redux-form]add redux validation

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -3,49 +3,46 @@ import { Field, reduxForm } from 'redux-form';
 import { PostEvent } from '../actions/post';
 import { Link, browserHistory } from 'react-router';
 import validate from './postValidate';
-const { DOM: { input } } = React
-
 
 class UpdateGame extends Component {
 
+
   onSubmit(props){
     console.log(props,'this is the props')
-    PostEvent(props)
-  }
 
+  }
+   renderInput = field =>{
+     console.log('fields of error',field.meta.error)
+   return(
+      <div>
+        <label>{field.label}</label>
+        <input {...field.input} placeholder={field.placeholder} type={field.type}/>
+        {field.touched && field.meta.error && <span>{field.meta.error}</span>}
+      </div>
+    )
+  }
 
   render(){
       const { handleSubmit } = this.props;
+
     return(
         <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-
-        <h3>Add Event</h3>
-
-          <div>
-            <Field name='location' component={location =>
+          <h3>Add Event</h3>
             <div>
-              <label>location</label>
-              <input type='text' {...location} placeholder='location'/>
-              {location.touched && location.error && <span className="error">{location.error}</span>}
+              <Field    className='form-control' name='location' component={this.renderInput} label='location' placeholder='location'/>
             </div>
-          }/>
-        </div>
 
-          <div>
-            <label>sport</label>
-            <Field name='sports' component='input' type='text'/>
-          </div>
+            <div>
+              <Field className='form-control' name='players' component={this.renderInput} label='players' placeholder='number' />
+            </div>
 
-          <div>
-            <label>players</label>
-            <Field name='players' component='input' type='integer'/>
-          </div>
-
-          <div>
-            <button type='submit' className='btn btn-primary'>Add</button>
-            <Link to='/map' className='btn btn-primary'>cancel</Link>
-          </div>
-
+            <div>
+              <Field className='form-control' name='sports' component={this.renderInput} label='sports' placeholder='sports'/>
+            </div>
+            <div>
+              <button type='submit' className='btn btn-primary'>Add</button>
+              <Link to='/map' className='btn btn-primary'>cancel</Link>
+            </div>
         </form>
     )
   }
