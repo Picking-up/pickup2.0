36b0a4045fb0671544c33664350017c61b22a97c
Jason Lee <jasonjlee.5@gmail.com>
2016-10-27 23:30:19 -0700

    [DB] Add user/event join table and also modify event to hold host

--- server/models/Users_Event.js
+++ server/models/Users_Event.js
@@ -2,13 +2,13 @@ var bookshelf = require('../config/bookshelf');
 var ModelBase = require('bookshelf-modelbase')(bookshelf);
 
 module.exports = (function() {
-  return bookshelf.model('Users', ModelBase.extend({
-    tableName: 'users',
-    userevents: function(){
-      return this.belongsTo('Users_Events');
-    },
-    userId: function(){
+  return bookshelf.model('Users_Events', ModelBase.extend({
+    tableName: 'userevents',
+    events: function(){
       return this.belongsTo('Events');
+    },
+    users: function(){
+      return this.belongsTo('Users');
     }
   }))
 })();
